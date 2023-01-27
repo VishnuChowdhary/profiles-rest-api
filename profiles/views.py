f46db8efc16f8a1c9ea0677272161bd56a8df615
VishnuChowdhary <112419086+VishnuChowdhary@users.noreply.github.com>
2023-01-27 13:55:46 +0530

    Filters added to profiles api

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
 from profiles import serializers
 from profiles import models
 from profiles import permissions
@@ -86,4 +87,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
     authentication_classes = (TokenAuthentication,)
-    permission_classes = [permissions.UpdateOwnProfile]
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
